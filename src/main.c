6efb692ca1084f044fe72edeab276784884aea3c
feepkl <maksymbiloskirka48@gmail.com>
2024-08-29 22:42:37 +0300

    main.c

--- src/main.c
+++ src/main.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 
-int main() {
+int main(){
 
-    int size = 5;
-    char symbol = '*';
-    
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            if (i == 0 || i == size - 1 || j == 0 || j == size - 1) {
-                printf("%c ", symbol);
-            } else {
-                printf("  "); 
-            }
-        }
-        printf("\n");
+    int a, b, c;
+
+    printf("Enter the first number: ");
+    scanf("%d", &a);
+
+    printf("Enter the second number: ");
+    scanf("%d", &b);
+
+    printf("Enter the third number: ");
+    scanf("%d", &c);
+
+    if(a > 0  b > 0  c > 0) {
+        printf("There is a positive number.\n");
+    } else {
+        printf("No number is positive");
     }
 
     return 0;
