c80e1e2512516d04939eec8296cb31abff3430cf
feepkl <maksymbiloskirka48@gmail.com>
2024-08-30 12:24:15 +0300

    main.c

--- src/main.c
+++ src/main.c
@@ -1,22 +1,17 @@
 #include <stdio.h>
 
-int main(){
+int main() {
+    int num;
 
-    int a, b, c;
+    printf("Enter a number: ");
+    scanf("%d", &num);
 
-    printf("Enter the first number: ");
-    scanf("%d", &a);
-
-    printf("Enter the second number: ");
-    scanf("%d", &b);
-
-    printf("Enter the third number: ");
-    scanf("%d", &c);
-
-    if(a > 0 || b > 0 || c > 0) {
-        printf("There is a positive number.\n");
-    } else {
-        printf("No number is positive");
+    if (num % 2 == 0) {
+        printf("The number is even.\n");
+    } else 
+    
+    if(!(num % 2 == 0)) {
+        printf("The number is odd.\n");
     }
 
     return 0;
