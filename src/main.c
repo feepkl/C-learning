28ca97969c6744b1733f7a79660b4b6f488b83de
feepkl <maksymbiloskirka48@gmail.com>
2024-10-29 20:50:09 +0200

    main.c

--- src/main.c
+++ src/main.c
@@ -1,15 +1,31 @@
 #include <stdio.h>
 
-int add(int a, int b) {
-    return a + b;
-}
-
 int main(){
-   int num1 = 10, num2 = 5;
- 
-   int addition = add(num1, num2);
+    int numbers1[5];
+    int numbers2[5];
 
-   printf("Result: %d\n", addition);
+    for(int i = 0; i < 5; i++) {
+       numbers1[i] = i + 1;
+    } 
+    for(int i = 0; i < 5; i++) {
+       numbers2[i] = i + 10;
+    }
 
-   return 0;
+    printf("Числа у першому масиві: \n");
+
+    for(int i = 0; i < 5; i++) {
+        printf ("%d\n", numbers1[i]);
+    }
+
+    printf("\n");
+
+    printf("Числа у другому масиві: \n");
+
+    for (int i = 0; i < 5; i++){
+        printf ("%d\n", numbers2[i]);
+    }
+
+    printf("\n");
+
+    return 0;
 }
